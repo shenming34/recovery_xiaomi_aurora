959376f73644c518e03e2814f62b1ffa1ecb2ac4
white <89283989+shenming34@users.noreply.github.com>
2024-04-29 22:31:48 +0800

    Add files via upload

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,10 +1,14 @@
-# Android device tree for Xiaomi 24031PN0DC (aurora)
-
-```
 #
 # Copyright (C) 2024 The Android Open Source Project
 # Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-```
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_aurora.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_aurora-user \
+    omni_aurora-userdebug \
+    omni_aurora-eng
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,10 @@
-# Android device tree for Xiaomi 24031PN0DC (aurora)
-
-```
 #
 # Copyright (C) 2024 The Android Open Source Project
 # Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-```
+
+add_lunch_combo omni_aurora-user
+add_lunch_combo omni_aurora-userdebug
+add_lunch_combo omni_aurora-eng
